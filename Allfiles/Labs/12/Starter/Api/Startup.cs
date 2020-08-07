8ae7d7aedd8e616d4166e79695a6709d1a127e99
Visweshwar Ganesh <vganesh@paychex.om>
2020-08-07 10:50:14 -0400

    12

---
+++ Allfiles/Labs/12/Starter/Api/Startup.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleApi {
+    public class Startup
+
+    {
+        private static string INSTRUMENTATION_KEY = "3bdc98b2-13ee-4808-ab74-65c0564f3189";
+        public Startup (IConfiguration configuration) {
+            //private static string INSTRUMENTATION_KEY = "3bdc98b2-13ee-4808-ab74-65c0564f3189";
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddControllers ();
+            services.AddApplicationInsightsTelemetry (INSTRUMENTATION_KEY);
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
+            }
+
+            //app.UseHttpsRedirection();
+
+            app.UseRouting ();
+
+            app.UseAuthorization ();
+
+            app.UseEndpoints (endpoints => {
+                endpoints.MapControllers ();
+            });
+        }
+    }
+}
